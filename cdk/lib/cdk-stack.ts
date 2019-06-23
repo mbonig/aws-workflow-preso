cd56a135b4e7527c9c26ff87ac7dc5366635cf9d
Matthew Bonig <matthew.bonig@gmail.com>
2019-06-22 20:55:45 -0600

    adding in api gateway to dynamodb

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,59 +1,131 @@
-import { Stack, Construct, StackProps } from '@aws-cdk/cdk';
-import { Bucket } from '@aws-cdk/aws-s3';
-import { Function, Code, Runtime, Tracing } from '@aws-cdk/aws-lambda';
-import { Table, AttributeType } from '@aws-cdk/aws-dynamodb';
-import { LambdaRestApi } from '@aws-cdk/aws-apigateway';
-import { PolicyStatement, PolicyStatementEffect } from '@aws-cdk/aws-iam';
+import {Construct, Stack, StackProps} from '@aws-cdk/cdk';
+import {Bucket, EventType} from '@aws-cdk/aws-s3';
+import {Code, Function, Runtime, Tracing} from '@aws-cdk/aws-lambda';
+import {AttributeType, Table} from '@aws-cdk/aws-dynamodb';
+import {
+    AwsIntegration,
+    LambdaRestApi,
+    PassthroughBehavior,
+    RestApi
+} from '@aws-cdk/aws-apigateway';
+import {PolicyDocument, PolicyStatement, Role, ServicePrincipal} from '@aws-cdk/aws-iam';
+import {S3EventSource} from '@aws-cdk/aws-lambda-event-sources';
 
 export class CdkStack extends Stack {
 
-  processLambda: Function;
-  s3Bucket: Bucket;
-  greedyApiGateway: LambdaRestApi;
-  docsTable: Table;
+    processLambda: Function;
+    docsBucket: Bucket;
+    greedyApiGateway: LambdaRestApi;
+    docsTable: Table;
+    catalogLambda: Function;
+    catalogReadGateway: RestApi;
+    private apigatewayRole: Role;
 
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
-    this.setupS3Bucket();
-    this.setupDynamoDB();
-    this.setupLambda();
-    this.setupAPIGateway();
-  }
+        this.setupS3Bucket();
+        this.setupDynamoDB();
+        this.setupLambda();
+        this.setupAPIGateway();
+    }
 
-  setupAPIGateway() {
+    setupAPIGateway() {
 
-    this.greedyApiGateway = new LambdaRestApi(this, 'endpoint', {
-      handler: this.processLambda
-    });
-  }
-  setupLambda() {
-    const dynamoAccess = new PolicyStatement(PolicyStatementEffect.Allow);
-    dynamoAccess.addResource(this.docsTable.tableArn);
-    dynamoAccess.addAction("dynamodb:*");
+        this.greedyApiGateway = new LambdaRestApi(this, 'endpoint', {
+            handler: this.processLambda
+        });
 
-    this.processLambda = new Function(this, 'process-doc', {
-      code: Code.directory('./handlers/convert'),
-      handler: 'AWSLambdas::AWSLambdas.Function::FunctionHandler',
-      runtime: Runtime.DotNetCore21,
-      environment: {
-        S3_BUCKET: this.s3Bucket.bucketName,
-        DOCS_TABLE: this.docsTable.tableName
-      },
-      initialPolicy: [dynamoAccess],
-      tracing: Tracing.Active
+        this.catalogReadGateway = new RestApi(this, 'catalog-api', {});
+        this.apigatewayRole = new Role(this, 'apigateway-dynamodb', {
+            assumedBy: new ServicePrincipal('apigateway.amazonaws.com'),
+            inlinePolicies: {
+                'read-dynamo': new PolicyDocument({
+                    statements: [
+                        new PolicyStatement({
+                            actions: ['dynamodb:*'],
+                            resources: [this.docsTable.tableArn]
+                        })]
+                })
+            }
+        });
+        this.catalogReadGateway.root.addMethod("GET", new AwsIntegration({
+            service: 'dynamodb',
+            action: 'Scan',
+            integrationHttpMethod: 'POST',
+            options: {
+                credentialsRole: this.apigatewayRole,
+                passthroughBehavior: PassthroughBehavior.WhenNoTemplates,
 
-    });
-  }
-  setupDynamoDB() {
-    this.docsTable = new Table(this, 'converted-docs', {
-      partitionKey: { name: 'docId', type: AttributeType.String }
-    })
-  }
-  setupS3Bucket() {
+                integrationResponses: [{
+                    statusCode: '200',
+                    responseTemplates: {
+                        'application/json': `#set($inputRoot = $input.path('$'))
+                            [
+                                #foreach($elem in $inputRoot.Items) {
+                                        "eTag": "$elem.eTag.S",
+                                        "key": "$elem.key.S",
+                                        "size": "$elem.size.N"
+                                    }#if($foreach.hasNext),#end
+                            \t#end
+                            ]`
+                    }
+                }],
 
-    this.s3Bucket = new Bucket(this, 'docs', {});
+                requestTemplates: {
+                    'application/json': `{
+                            "TableName": "CdkStack-docscatalog229FB2BA-13C1W6I45GIKM"
+                    }`
+                },
 
+            }
+        }), {
+            methodResponses: [{
+                statusCode: '200'
+            }]
+        });
+    }
 
-  }
+    setupLambda() {
+
+        this.processLambda = new Function(this, 'process-doc', {
+            code: Code.asset('./handlers/convert'),
+            handler: 'AWSLambdas::AWSLambdas.Function::FunctionHandler',
+            runtime: Runtime.DotNetCore21,
+            environment: {
+                S3_BUCKET: this.docsBucket.bucketName
+            },
+            tracing: Tracing.Active
+        });
+
+        const dynamoAccess = new PolicyStatement({
+            actions: ["dynamodb:*"],
+            resources: [this.docsTable.tableArn],
+        });
+        this.catalogLambda = new Function(this, 'catalog-doc', {
+            code: Code.asset('./handlers/catalog'),
+            handler: 'index.handler',
+            runtime: Runtime.Nodejs810,
+            environment: {
+                DOCS_TABLE: this.docsTable.tableName
+            },
+            initialPolicy: [dynamoAccess],
+
+        });
+
+        this.catalogLambda.addEventSource(new S3EventSource(this.docsBucket, {
+            events: [EventType.ObjectCreatedPut]
+        }));
+
+    }
+
+    setupDynamoDB() {
+        this.docsTable = new Table(this, 'docs-catalog', {
+            partitionKey: {name: 'docId', type: AttributeType.String}
+        })
+    }
+
+    setupS3Bucket() {
+        this.docsBucket = new Bucket(this, 'docs', {});
+    }
 }
